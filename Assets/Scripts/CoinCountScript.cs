8ade9b82f9dad518ac6229237a0e91c7f5531a64
[pali-s] <[palishma077@academiacollege.edu.np]>
2024-12-01 18:12:00 +0545

    shop improve

--- Assets/Scripts/CoinCountScript.cs
+++ Assets/Scripts/CoinCountScript.cs
@@ -8,11 +8,13 @@ public class CoinCountScript : MonoBehaviour
     public int coinCount;
     public Text coinCounttext;
 
+    public GameObject alertDisplay;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        alertDisplay.SetActive(false);
         LoadCat();
     }
 
@@ -39,11 +41,23 @@ public class CoinCountScript : MonoBehaviour
 
     public void expense(int cost)
     {
+
         if (coinCount > cost)
         {
             InventoryScript.Instance.SpendCoin(10);
             LoadCat();
+           
+        }
+        else
+        {
+            alertDisplay.SetActive(true);
+            Invoke("hidealert", 2);
         }
     }
+    public void hidealert()
+    {
+        alertDisplay.SetActive(false);
+    }
+
 }
 
